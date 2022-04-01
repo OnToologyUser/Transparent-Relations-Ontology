44d8fe80cc6b0caf88bdc2b0067b39aa025a9d55
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2022-04-01 14:37:14 +0200

    Widoco release

---
+++ widoco.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+docker run -ti --rm \
+  -v `pwd`/development:/usr/local/widoco/in \
+  -v `pwd`/release:/usr/local/widoco/out \
+  dgarijo/widoco -ontFile in/TransparentRelationsOntology.ttl -outFolder out -getOntologyMetadata -rewriteAll -htaccess -includeAnnotationProperties -uniteSections
+
+
